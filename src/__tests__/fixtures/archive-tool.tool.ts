bd4f1d894210890881ed526c1d4c58ac0b9eed33
Alex Gorbatchev <alex.gorbatchev@gmail.com>
2025-08-12 18:54:09 -0700

    Fix typecheck

--- src/__tests__/fixtures/archive-tool.tool.ts
+++ src/__tests__/fixtures/archive-tool.tool.ts
@@ -1,6 +1,6 @@
 import type { ToolConfigBuilder, ToolConfigContext } from '@types';
 
-export default async (c: ToolConfigBuilder, ctx: ToolConfigContext): Promise<void> => {
+export default async (c: ToolConfigBuilder, _ctx: ToolConfigContext): Promise<void> => {
   c.bin('archive-tool').version('1.0.0').install('github-release', {
     repo: 'mock-owner/archive-repo',
     assetPattern: 'archive-tool-v1.0.0-linux-amd64.tar.gz',
