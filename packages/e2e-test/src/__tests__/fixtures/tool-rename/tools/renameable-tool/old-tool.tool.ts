6bc9a4907b8f5eeb129ecdbd6b4863cd9a4e3801
Alex Gorbatchev <alex.gorbatchev@gmail.com>
2026-04-10 13:24:02 -0700

    style: format workflow and fixture tool files

--- packages/e2e-test/src/__tests__/fixtures/tool-rename/tools/renameable-tool/old-tool.tool.ts
+++ packages/e2e-test/src/__tests__/fixtures/tool-rename/tools/renameable-tool/old-tool.tool.ts
@@ -1,3 +1,5 @@
-import { defineTool } from "@dotfiles/cli";
+import { defineTool } from '@dotfiles/cli';
 
-export default defineTool((install) => install().symlink("./config.txt", "~/.config/renameable-tool/config.txt"));
+export default defineTool((install) =>
+  install().symlink('./config.txt', '~/.config/renameable-tool/config.txt')
+);
